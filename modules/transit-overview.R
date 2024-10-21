c52d9cbed15b38fece5800e05867cbec716fcbcd
Craig Helmann <chelmann@psrc.org>
2024-10-21 10:27:22 -0700

    added carousel to overview page

--- modules/transit-overview.R
+++ modules/transit-overview.R
@@ -20,6 +20,25 @@ transit_overview_server <- function(id) {
         br(),
         textOutput(ns("transit_overview_text")),
         br(),
+        
+        card(
+          
+          height = "450px",
+          
+          bs_carousel(id = "transit_photos", use_indicators = TRUE, use_controls = TRUE) |>
+            bs_append(content = bs_carousel_image(src = "ct.jpg")) |>
+            bs_append(content = bs_carousel_image(src = "kc-fast-ferry.jpg")) |>
+            bs_append(content = bs_carousel_image(src = "et.jpg")) |>
+            bs_append(content = bs_carousel_image(src = "line1.jpg")) |>
+            bs_append(content = bs_carousel_image(src = "wsf.jpg")) |>
+            bs_append(content = bs_carousel_image(src = "kcm.jpg")) |>
+            bs_append(content = bs_carousel_image(src = "pt.jpg")) |>
+            bs_append(content = bs_carousel_image(src = "slu.jpg")) |>
+            bs_append(content = bs_carousel_image(src = "line2.jpg")) |>
+            bs_append(content = bs_carousel_image(src = "kt-fast-ferry.jpg")) |>
+            bs_append(content = bs_carousel_image(src = "sounder.jpg"))
+          
+        )
       )
     })
   })  # end moduleServer
