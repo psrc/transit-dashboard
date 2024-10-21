3b18f29147fd2e755015c418b2bf2f4bb9417486
Craig Helmann <chelmann@psrc.org>
2024-10-21 14:09:12 -0700

    updated overview tab with transit carousel

--- modules/transit-overview.R
+++ modules/transit-overview.R
@@ -12,31 +12,38 @@ transit_overview_server <- function(id) {
     ns <- session$ns
     
     # Text
-    output$transit_overview_text <- renderText({page_information(tbl=page_text, page_name="Transit", page_section = "Overview", page_info = "description")})
+    output$transit_overview_text <- renderUI({HTML(page_information(tbl=page_text, page_name="Transit", page_section = "Overview", page_info = "description"))})
+    output$transit_hct_text <- renderUI({HTML(page_information(tbl=page_text, page_name="Transit", page_section = "Overview-HCT", page_info = "description"))})
     
     # Overview UI
     output$transitoverview <- renderUI({
       tagList(
         br(),
-        textOutput(ns("transit_overview_text")),
+        htmlOutput(ns("transit_overview_text")),
         br(),
         
         card(
           
-          height = "450px",
+          card_body(
+            layout_column_wrap(
+              width= "300px",
+              htmlOutput(ns("transit_hct_text")),
+              bs_carousel(id = "transit_photos", use_indicators = TRUE, use_controls = TRUE) |>
+                bs_append(content = bs_carousel_image(src = "ct_adj.jpg")) |>
+                bs_append(content = bs_carousel_image(src = "kc-fast-ferry_adj.jpg")) |>
+                bs_append(content = bs_carousel_image(src = "et_adj.jpg")) |>
+                #bs_append(content = bs_carousel_image(src = "line1_adj.jpg")) |>
+                bs_append(content = bs_carousel_image(src = "wsf_adj.jpg")) |>
+                bs_append(content = bs_carousel_image(src = "kcm_adj.jpg")) |>
+                bs_append(content = bs_carousel_image(src = "pt_adj.jpg")) |>
+                #bs_append(content = bs_carousel_image(src = "slu_adj.jpg")) |>
+                bs_append(content = bs_carousel_image(src = "line2_adj.jpg")) |>
+                bs_append(content = bs_carousel_image(src = "kt-fast-ferry_adj.jpg")) |>
+                bs_append(content = bs_carousel_image(src = "sounder_adj.jpg"))
+            )
+          )
           
-          bs_carousel(id = "transit_photos", use_indicators = TRUE, use_controls = TRUE) |>
-            bs_append(content = bs_carousel_image(src = "ct.jpg")) |>
-            bs_append(content = bs_carousel_image(src = "kc-fast-ferry.jpg")) |>
-            bs_append(content = bs_carousel_image(src = "et.jpg")) |>
-            bs_append(content = bs_carousel_image(src = "line1.jpg")) |>
-            bs_append(content = bs_carousel_image(src = "wsf.jpg")) |>
-            bs_append(content = bs_carousel_image(src = "kcm.jpg")) |>
-            bs_append(content = bs_carousel_image(src = "pt.jpg")) |>
-            bs_append(content = bs_carousel_image(src = "slu.jpg")) |>
-            bs_append(content = bs_carousel_image(src = "line2.jpg")) |>
-            bs_append(content = bs_carousel_image(src = "kt-fast-ferry.jpg")) |>
-            bs_append(content = bs_carousel_image(src = "sounder.jpg"))
+          
           
         )
       )
