ed5502d7ee9c686b8ff11d6d818b27ee0aa56363
Craig Helmann <chelmann@psrc.org>
2024-11-01 09:22:19 -0700

    dev deployment added to deploy file

--- deploy.R
+++ deploy.R
@@ -5,3 +5,10 @@ library(rsconnect)
 deployApp(account = 'psrcwa',
          appName = 'transit-dashboard',
          appTitle = 'Transit Data Dashboard')
+
+# Development version -----------------------------------------------------
+
+deployApp(account = 'psrcwa',
+         appName = 'transit-dashboard-dev',
+         appTitle = 'Dev Version Transit Data Dashboard')
+ 
