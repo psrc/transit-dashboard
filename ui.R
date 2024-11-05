7bb785843fe3ab675b69cc3a678ccfee589b6a82
Craig Helmann <chelmann@psrc.org>
2024-11-05 10:56:31 -0800

    added language tag to the ui

--- ui.R
+++ ui.R
@@ -1,41 +1,46 @@
 shinyUI(
   
-  page_navbar(
+  tags$html(
     
-    # JavaScript to modify tabindex values to 0 so you can tab to them
-    tags$script(HTML("
-    $(document).ready(function() {
-      function setTabindex() {
-        $('.nav-link').attr('tabindex', '0');
-      }
-      // Set tabindex initially
-      setTabindex();
-
-      // Listen for tab changes and reset tabindex
-      $('.nav-link').on('shown.bs.tab', function() {
+    lang = "en",
+    
+    page_navbar(
+    
+      # JavaScript to modify tabindex values to 0 so you can tab to them
+      tags$script(HTML("
+      $(document).ready(function() {
+        function setTabindex() {
+          $('.nav-link').attr('tabindex', '0');
+        }
+        // Set tabindex initially
         setTabindex();
+  
+        // Listen for tab changes and reset tabindex
+        $('.nav-link').on('shown.bs.tab', function() {
+          setTabindex();
+        });
       });
-    });
-  ")),
+    ")),
     
-    position = c("static-top"),
+      position = c("static-top"),
     
-    title = tags$a(div(tags$img(src='psrc-logo.png', style="margin-top: 10px; padding-left: 20px; padding-right: 30px;", height = "65", alt = "Link to PSRC Homepage")), href="https://www.psrc.org", target="_blank"),
-    fillable = FALSE,
-    theme = psrc_theme,
-  
-    nav_panel("Overview", transit_overview_ui('OVERVIEWtransit')),
-    nav_panel("Region", transit_region_ui('REGIONtransit')),
-    nav_panel("Mode", transit_mode_ui('MODEtransit')),
-    nav_panel("Operator", transit_operator_ui('OPERATORtransit')),
-    nav_panel("Type", transit_type_ui('TYPEtransit')),
-    nav_panel("Frequency", transit_trips_ui('TRIPtransit')),
-    nav_panel("Routes", transit_route_ui('ROUTEtransit')),
+      title = tags$a(div(tags$img(src='psrc-logo.png', style="margin-top: 10px; padding-left: 20px; padding-right: 30px;", height = "65", alt = "Link to PSRC Homepage")), href="https://www.psrc.org", target="_blank"),
+      fillable = FALSE,
+      theme = psrc_theme,
+
+      nav_panel("Overview", transit_overview_ui('OVERVIEWtransit')),
+      nav_panel("Region", transit_region_ui('REGIONtransit')),
+      nav_panel("Mode", transit_mode_ui('MODEtransit')),
+      nav_panel("Operator", transit_operator_ui('OPERATORtransit')),
+      nav_panel("Type", transit_type_ui('TYPEtransit')),
+      nav_panel("Frequency", transit_trips_ui('TRIPtransit')),
+      nav_panel("Routes", transit_route_ui('ROUTEtransit')),
     
-    br(), br(),
+      br(), br(),
     
-    footer = (footer_ui('psrcfooter'))
-  
-  ) # end of page_navbar
+      footer = (footer_ui('psrcfooter'))
+      
+      ) # end of page_navbar
+  ) # end of HTML tag for UI
 ) # end of Shiny App
 
