583ad5ece2bd121b76a19e7d35ecdc2c741e2bb9
Craig Helmann <chelmann@psrc.org>
2024-11-06 14:55:41 -0800

    updated region heading creation with a default required on rendering

--- server.R
+++ server.R
@@ -7,8 +7,16 @@ shinyServer(function(input, output) {
   output$transit_howto_text <- renderUI({HTML(page_information(tbl=page_text, page_name="Transit", page_section = "Overview-HowTo", page_info = "description"))})
   
   # Region Page
-  output$region_page_title <- renderText(paste0(input$RegionMetric, " for all Transit Modes"))
-  output$region_chart_title <- renderText(paste0("Regional ",input$RegionMetric))
+  output$region_page_title <- renderText({
+    req(input$RegionMetric)
+    paste0(input$RegionMetric, " for all Transit Modes")
+    })
+  
+  output$region_chart_title <- renderText({
+    req(input$RegionMetric)
+    paste0("Regional ",input$RegionMetric)
+    })
+  
   output$region_insights_text <- renderUI({HTML(page_information(tbl=page_text, page_name="Transit", page_section = "Region", page_info = "description"))})
   value_box_server('REGIONvaluebox', df=ntd_data, m=reactive(input$RegionMetric), v=reactive("All Transit Modes"), g="Region", gt="Region", gr=paste0("Year to Date: Jan-",latest_ntd_month))
   bar_chart_server('REGIONbarchart', df=ntd_data, m=reactive(input$RegionMetric), v=reactive("All Transit Modes"), g="Region", gt="Region", color = c("#00A7A0"))
